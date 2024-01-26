bc564abed42a0b905885ac00514a7b2ae1d7793c
Deepank481 <pant.deepank045@gmail.com>
2024-01-27 01:16:07 +0530

    27 jan

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 
 //Three Accounts created
 const account1 = {
-  fName: "Deepank",
-  lName: "Pant",
+  fullName: "Deepank Pant",
   accountNo: "0000012345",
   balance: 0,
   summary: [0, 0],
@@ -12,8 +11,7 @@ const account1 = {
 };
 
 const account2 = {
-  fName: "Aditya",
-  lName: "Dev",
+  fullName: "Aditya Dev",
   accountNo: "0000054321",
   balance: 0,
   summary: [0, 0],
@@ -22,8 +20,7 @@ const account2 = {
 };
 
 const account3 = {
-  fName: "Piyush",
-  lName: "Lohani",
+  fullName: "Piyush Lohani",
   accountNo: "0000067890",
   balance: 0,
   summary: [0, 0],
@@ -31,18 +28,33 @@ const account3 = {
   interestRate: 3.5,
 };
 
+const accounts = [account1, account2, account3];
+
 //Fetching elements
 const transactionList = document.getElementById("transaction-list");
 
 function displayUserData(transactionHistory) {
+  transactionList.innerHTML = "";
   transactionHistory.forEach(function (trans, i) {
     const color = trans > 0 ? "green" : "red";
-    const html = `<li class="transaction-item"><span style="color: ${color};">Transaction ${
+    const html = `<li class="transaction-item"><pre style="color: ${color};">Transaction ${
       i + 1
-    }     ${trans}</span></li>`;
+    }       ${trans}</pre></li>`;
 
     transactionList.insertAdjacentHTML("afterbegin", html);
   });
 }
 
+const createUserName = function (acc) {
+  acc.forEach((account) => {
+    account.userName = account.fullName
+      .toLowerCase()
+      .split(" ")
+      .map((val) => val[0])
+      .join("");
+  });
+};
+
 displayUserData(account1.transactionHistory);
+createUserName(accounts);
+console.log(accounts);
